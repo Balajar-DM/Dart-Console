b606c4c6fc0647bd781c5ed86040cc0ae5dd8236
DionMontolalu <dionmontolalu@gmail.com>
2021-11-27 14:15:25 +0800

    function, optional parameter, lamda expression

--- bin/function.dart
+++ bin/function.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 //function standart
 double luas_segiempat(double panjang, double lebar) {
   double hasil;
@@ -14,10 +16,27 @@ String say(String from, String message, {String to, String appName}) {
       ((appName != null) ? 'via' + appName : '');
 }
 
+//Lamda Expression untuk lebih mempersingkat penulisan kode
+double luas_segiempat2(double panjang, double lebar) => panjang * lebar;
+
+//Anonymous function
+int doMathOperator(int number1, int number2, Function(int, int) operator) {
+  return operator(number1, number2);
+}
+
 void main(List<String> arguments) {
-  /*double p, l;
+  //cara menggunakan method luas_segiempat
+  double p, l;
   p = double.tryParse(stdin.readLineSync());
   l = double.tryParse(stdin.readLineSync());
-  print(luas_segiempat(p, l));*/
-  print(say('Johny', 'Hello'));
+  print(luas_segiempat(p, l));
+
+  //cara menggunakan method say yang ada optional parameter
+  print(say('Tasya', 'Hello'));
+
+  //cetak method lambda expression
+  print(luas_segiempat2(12, 3));
+
+  //cetak data anonymous function
+  print(doMathOperator(1, 2, (a, b) => a * b));
 }
